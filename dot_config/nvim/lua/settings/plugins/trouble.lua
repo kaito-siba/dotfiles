d45649ec8ee17fc7d081bdad1eff6a3e0cbec4b9
k-nanchi <kaito@siba-service.jp>
2024-06-19 19:39:58 +0900

    🐛 fix trouble setting

--- dot_config/nvim/lua/settings/plugins/trouble.lua
+++ dot_config/nvim/lua/settings/plugins/trouble.lua
@@ -1,11 +1,38 @@
 return {
 	"folke/trouble.nvim",
+	opts = {},
 	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+	cmd = "Trouble",
 	keys = {
-		{ "<leader>tw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-		{ "<leader>tf", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-		{ "<leader>tq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-		{ "<leader>tl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
-		{ "<leader>tt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
 	},
 }
