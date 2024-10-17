4a3a4973460c7ac14687adec19c0d8505f14479e
k-nanchi <kaito@siba-service.jp>
2024-10-17 19:50:23 +0900

    🔧 configure wezterm tab

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -30,6 +30,108 @@ config.scrollback_lines = 99999
 config.window_decorations = "RESIZE"
 config.show_new_tab_button_in_tab_bar = false
 
+config.window_frame = {
+	font = wezterm.font("UDEV Gothic 35JPDOC", { weight = "Bold" }),
+	font_size = 12,
+	inactive_titlebar_bg = "none",
+	active_titlebar_bg = "none",
+}
+
+config.colors = {
+	tab_bar = {
+		inactive_tab_edge = "none",
+	},
+}
+
+config.show_close_tab_button_in_tabs = false
+
+-- 現ディレクトリとgitブランチ名を取得
+local function set_title(pane)
+	local cwd_uri = pane:get_current_working_dir()
+
+	local cwd_uri_string = wezterm.to_string(cwd_uri)
+	local cwd = cwd_uri_string:gsub("^file://", "")
+
+	if not cwd then
+		return nil
+	end
+
+	-- Gitのブランチ名を取得
+	local success, stdout, stderr = wezterm.run_child_process({
+		"git",
+		"-C",
+		cwd,
+		"branch",
+		"--show-current",
+	})
+
+	local current_dir = cwd:match("([^/]+)/?$")
+
+	local ret = current_dir
+
+	-- Gitブランチ名を取得できたら「ブランチ名:ディレクトリ名」と表示できるようにする
+	if success then
+		local branch = stdout:gsub("%s+", "")
+		ret = branch .. ":" .. current_dir
+	end
+
+	return ret
+end
+
+-- 各タブの「ブランチ名:ディレクトリ名」を記憶しておくテーブル
+local title_cache = {}
+
+-- 各タブ（正確にはpane）に「ブランチ名:ディレクトリ名」を記憶させる
+wezterm.on("update-status", function(window, pane)
+	local title = set_title(pane)
+	local pane_id = pane:pane_id()
+
+	title_cache[pane_id] = title
+end)
+
+-- タブの左側の装飾
+local LEFT_CIRCLE = wezterm.nerdfonts.ple_left_half_circle_thick
+-- タブの右側の装飾
+local RIGHT_CIRCLE = wezterm.nerdfonts.ple_right_half_circle_thick
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local background = "#5c6d74"
+	local foreground = "#FFFFFF"
+
+	if tab.is_active then
+		background = "#9EC0FE"
+		foreground = "#FFFFFF"
+	end
+
+	local edge_background = "none"
+	local edge_foreground = background
+
+	local pane = tab.active_pane
+	local pane_id = pane.pane_id
+	local cwd_git
+
+	-- 記憶させていた「ブランチ名:ディレクトリ名」を取り出す
+	if title_cache[pane_id] then
+		cwd_git = title_cache[pane_id]
+	else
+		cwd_git = tab.active_pane.title
+	end
+
+	local title = " " .. cwd_git .. " "
+
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = LEFT_CIRCLE },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = RIGHT_CIRCLE },
+	}
+end)
+
 -- 最初からフルスクリーンで起動
 local mux = wezterm.mux
 wezterm.on("gui-startup", function(cmd)
