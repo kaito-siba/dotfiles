45b64c2f567c40d0f19a2e175b5ea3770658e9e0
kaito-siba <kaito@siba-serive.jp>
2024-10-26 18:47:29 +0900

    🩹 update sketchybar theme to cappuccin

--- dot_config/sketchybar/executable_colors.sh
+++ dot_config/sketchybar/executable_colors.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# export PURPLE=0xFFC099FF
-export PURPLE=0x292A96FA
-export GREY=0x222436FF
+export BASE=0xffeff1f5
+export LAVENDER=0xff7287fd
+export TEXT=0xff4c4f69
+export OVERLAY0=0xffacb0be
+export OVERLAY1=0xff9ca0b0
+export SURFACE0=0xffccd0da
+export SURFACE1=0xffbcc0cc
+export MANTLE=0xffe6e9ef
 export TRANSPARENT=0x00000000
-export WHITE=0xFFFFFFFF
-export BLUE=0x292E42FF
-export MAGENTA=0xFF007CFF
-export ORANGE=0xFF966CFF
-export TEMPUS=0xFF1E2030
-export STATUS=0xFF394B70
-export SPACEBG=0xAA98A8EB
-export MIDNIGHT=0xFF3D59A1
